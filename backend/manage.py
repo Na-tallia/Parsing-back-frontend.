2f63ff3277e0eef2acc49a38bc615f45e3676add
Na-tallia <mlz.kng@gmail.com>
2026-01-27 22:00:49 +0300

    изменен файл manage.py т.к. папка api сейчас лежит в корне

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,19 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # Получаем путь к папке backend/
+    current_path = os.path.abspath(os.path.dirname(__file__))
+    # Получаем путь к корню проекта (на одну папку выше)
+    project_root = os.path.dirname(current_path)
+
+    # Добавляем обе папки в пути поиска модулей Python
+    # Это позволит Django увидеть папку 'api', даже если она в корне
+    sys.path.insert(0, current_path)
+    sys.path.insert(0, project_root)
+
+    # Указываем Django, где искать файл настроек.
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
